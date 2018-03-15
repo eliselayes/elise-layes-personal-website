e2510475bb3c2c752a0f1e3418dfcd82ea99df66
AkeraKyuar <elise-layes@laposte.net>
2018-03-15 13:24:48 +0100

    tools and contact template added

--- view/frontend/homeView.php
+++ view/frontend/homeView.php
@@ -1,47 +1,47 @@
 <?php $title = 'Accueil'; ?>
 
 <?php ob_start(); ?>
-<?php include("tools.php"); ?>
-<?php include("contact.php"); ?>
+<?php include("templates/tools.php"); ?>
+<?php include("templates/contact.php"); ?>
 
 <div class="jumbotron jumbotronPres">
-    <div class="container presentation">
+    <div class="container website-container presentation">
         <img id="star1" src="public/images/star.svg" alt="">
         <div class="col-sm-4 col-sm-offset-4 colPres">
             <h2>Elise Layes</h2>
             <p>Développeuse web junior</p>
         </div>
     </div>
-    <div class="container nav">
+    <div class="container website-container nav">
         <img id="star2" src="public/images/star2.png" alt="">
         <div class="row apropos">
-            <div class="col-sm-2 col-sm-offset-5">
+            <div class="col-sm-2 col-sm-offset-5 colCont">
                 <a href="index.php?action=about">A PROPOS</a>
             </div>
         </div>
 
         <div class="row portfolio">
-            <div class="col-sm-2 col-sm-offset-5">
+            <div class="col-sm-2 col-sm-offset-5 colCont">
                 <a href="index.php?action=portfolio">PORTFOLIO</a>
             </div>
         </div>
 
         <div class="row outils">
-            <div class="col-sm-2 col-sm-offset-5">
+            <div class="col-sm-2 col-sm-offset-5 colCont">
                 <a class="outils" href="#">OUTILS</a>
             </div>
         </div>
 
-        <div class="row contact">
-            <div class="col-sm-2 col-sm-offset-5">
-                <a class="contact">BLOG</a>
+        <div class="row blog">
+            <div class="col-sm-2 col-sm-offset-5 colCont">
+                <a href="index.php?action=blog">BLOG</a>
             </div>
         </div>
         <div class=" empty">
-            <div class="bordRight2"></div>
-            <div class="bordLeft2"></div>
+            <div class="bordRight"></div>
+            <div class="bordLeft"></div>
         </div>
-        <div class="row blog">
+        <div class="row contact">
             <div class="col-sm-12">
                 <p><a href ="#" class="contactWin">Contact</a></p>
                 <p><img id="star3" src="public/images/star2.png" alt=""></p>
@@ -52,4 +52,4 @@
 <?php $content = ob_get_clean(); ?>
 
 
-<?php require('template.php'); ?>
+<?php require('templates/template.php'); ?>
