2fc2fe6dc850f4a5f97f0afbfab6093187090428
AkeraKyuar <elise-layes@laposte.net>
2018-03-15 13:22:01 +0100

    logView added

---
+++ view/frontend/logView.php
@@ -0,0 +1,23 @@
+<?php $title = 'Se connecter'; ?>
+
+<?php ob_start(); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-sm-offset-3 connexion">
+            <h2>Connexion</h2>
+            <form class="log" action="index.php?action=mainBackend" method="post">
+                <div class="containElmForm"><label for="pseudo">Pseudo :</label><input type="text" name="pseudo" id="pseudo" /></div>
+                <div class="containElmForm"><label for="pass">Mot de passe :</label><input type="text" name="pass" id="pass" /></div><br />
+                <div class="containElmForm submit"><input id="login" type="submit" value="Se connecter" /></div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+<?php $content = ob_get_clean(); ?>
+
+<?php ob_start(); ?>
+<?php $sidebar = ob_get_clean(); ?>
+
+<?php require('templates/blog.php'); ?>
