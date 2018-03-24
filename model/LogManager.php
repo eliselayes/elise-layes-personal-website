4fecd729a829b58cdebbddb247501162e64459ad
AkeraKyuar <elise-layes@laposte.net>
2018-03-24 22:09:04 +0100

    function getPass changed and function getName added

--- model/LogManager.php
+++ model/LogManager.php
@@ -12,12 +12,20 @@ class LogManager extends Manager
         $result = $req->fetch();
     }
 
-    public function getPass() {
-        $sql = 'SELECT pass FROM user WHERE name = "Elise"';
-        $req = $this->executeRequest($sql, array());
+    public function getPass($name) {
+        $sql = 'SELECT pass FROM user WHERE name = ?';
+        $req = $this->executeRequest($sql, array($name));
         $pass = $req->fetch();
         return $pass['pass'];
     }
+    
+    public function getName($pass) {
+        $sql = 'SELECT name FROM user WHERE pass = ?';
+        $req = $this->executeRequest($sql, array($pass));
+        $name = $req->fetch();
+        
+    }
+
 
     public function changePass($pass) {
         $pass_hash = password_hash($pass, PASSWORD_DEFAULT);
