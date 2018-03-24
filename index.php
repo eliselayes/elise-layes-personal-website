7dd44cfe124abc7a027f02193adf5001381ca969
AkeraKyuar <elise-layes@laposte.net>
2018-03-24 21:17:46 +0100

    autoload twig added

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php 
-require_once('model/Router.php');
 
-$router = new \site_perso\Model\Router();
+require_once ('vendor/autoload.php');
+require_once('Router.php');
+
+/*use \Twig_Loader_Filesystem;
+use \Twig_Environment;*/
+
+/*$loader = new Twig_Loader_Filesystem(array('view/backend/templates', 'view/frontend/templates'));
+$twig = new Twig_Environment($loader, array(
+    'cache' => false,
+));
+
+echo $twig->render('menu.php');*/
+
+$router = new \site_perso\Controller\Router();
 $router->direct();
