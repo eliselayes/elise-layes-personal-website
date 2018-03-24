8d7de1a8b339cf43e9f8633ad13e2e9e93a27b7b
AkeraKyuar <elise-layes@laposte.net>
2018-03-24 21:40:49 +0100

    JVView added

---
+++ view/frontend/JVView.php
@@ -0,0 +1,78 @@
+<?php $title = 'Liste des articles'; ?>
+
+<?php ob_start(); ?>
+
+    <div class="blog-post">
+    <?php
+        while ($data = $posts->fetch()) {
+            if($data['category'] == "Jeux vidéo") {
+            ?>
+                <h2 class="blog-post-title"><?= $data['title']; ?></h2>
+                <p class="blog-post-meta"><?= $data['creation_date_fr']; ?></p>
+                <p><?= html_entity_decode($data['short_content']); ?>...</p>
+                <a href="index.php?action=seeOnePost&amp;id=<?= $data['id'] ?>">  lire la suite</a>
+    
+    <?php
+            }
+        }
+        $posts->closeCursor();
+    ?>
+    </div><!-- /.blog-post -->
+    <nav class="pager">
+        <?php
+            if($cPage != 1) {
+                $prev=$cPage-1;
+        ?>
+            <form action="index.php?action=changePageJV&amp;page=<?= $prev ?>" method="post">
+                <input type="submit" value="Précédent">
+            </form>
+        <?php
+        }
+            if($cPage < $totalPages) {
+                $next=$cPage+1;
+        ?>
+            <form action="index.php?action=changePageJV&amp;page=<?= $next ?>" method="post">
+                <input type="submit" value="Suivant">
+            </form>
+
+        <?php
+        }
+        ?>
+    </nav>
+
+<?php $content = ob_get_clean(); ?>   
+
+
+
+
+
+
+<?php ob_start(); ?>
+    <div class="sidebar-module">
+        <h4>Archives</h4>
+        <ol class="list-unstyled">
+            <?php
+              while ($data = $months->fetch()) {
+            ?>
+                <li><a href="index.php?action=seePostsPerMonth&amp;m=<?= $data['m'] ?>"><?= $data['mois']; ?> <?= $data['y']; ?></a></li>
+            <?php
+              }
+            $months->closeCursor();
+            ?>
+        </ol> 
+    </div>
+    <div class="sidebar-module">
+        <h4>Liens</h4>
+        <ol class="list-unstyled">
+            <li><a href="#">GitHub</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Facebook</a></li>
+        </ol>
+    </div> <!-- sidebar -->
+        
+<?php $sidebar = ob_get_clean(); ?>
+      
+
+
+<?php require('templates/blog.php'); ?>
+
