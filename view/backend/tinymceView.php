4b5c88c4a9a97cb665fc035b27a5d65161a3619c
AkeraKyuar <elise-layes@laposte.net>
2018-03-24 21:47:21 +0100

    title and category added, blog style css added

--- view/backend/tinymceView.php
+++ view/backend/tinymceView.php
@@ -20,6 +20,7 @@
 
     <!-- Custom styles for this template -->
     <link href="public/css/style.css" rel="stylesheet">
+    <link href="public/css/blog-style.css" rel="stylesheet">
 
 
       <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
@@ -27,20 +28,20 @@
         selector:'textarea',
         language_url : 'public/js/tinymce/langs/fr_FR.js',
         language: 'fr_FR',
-        plugins: "save",
-        toolbar: "save"
       });</script>
     </head>
 
     <header id="header-editor">
       <h1 id="editor-title">Bienvenue !</h1>
-      <p class="editor-description">Quoi de neuf aujourd'hui ?</p>
     </header>
 
     <body id="editor">
       <form action="index.php?action=sendText" method="post">
+        <label class="containElmForm" for="title">Titre :</label><input type="text" name="title" id="title" /><br />
+        <label class="containElmForm" for="category">Catégorie :</label><input type="text" name="category" id="category" /><br />
         <textarea name="content" id="content"></textarea>
+        <input class="containElmForm" id="sendPost" type="submit" value="Enregistrer" />
       </form>
-      <p id="editor-back"><a href="index.php?action=blog">Retour au blog</a></p>     
+      <p id="editor-back"><a href="index.php?action=editText">Retour</a></p>     
   </body>
 </html>
